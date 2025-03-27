f678480589c728783a059b26e037400a3363337b
Ian <ian.c.bacher@gmail.com>
2025-03-27 13:09:58 -0400

    Fix sushi name and properly comment

--- _genoncedocker.bat
+++ _genoncedocker.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-# Build docker image tagged 'openelis-ig-publisher'
+REM Build docker image tagged 'openelis-ig-publisher'
 docker build -t openelis-ig-publisher .
 
-# Run docker container mounting the current directory as /app, passing any arguments
+REM Run docker container mounting the current directory as /app, passing any arguments
 docker run --rm -v "%CD%/output":/ig/output openelis-ig-publisher "%*"
