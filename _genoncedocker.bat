c725e3000648f3e2dc8425c41d8bf89a839aa460
Ian <ian.c.bacher@gmail.com>
2025-03-27 13:01:01 -0400

    Small tweaks for Windows

--- _genoncedocker.bat
+++ _genoncedocker.bat
@@ -1,5 +1,7 @@
+@ECHO OFF
+
 # Build docker image tagged 'openelis-ig-publisher'
 docker build -t openelis-ig-publisher .
 
 # Run docker container mounting the current directory as /app, passing any arguments
-docker run --rm -v "$PWD/output":/ig/output openelis-ig-publisher "%*"
+docker run --rm -v "%CD%/output":/ig/output openelis-ig-publisher "%*"
